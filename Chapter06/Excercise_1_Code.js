de56efa9ea68c1108904337cf5bcbe05c91306a6
rachanakaria <44355789+rachanakaria@users.noreply.github.com>
2019-11-13 11:17:44 +0530

    moved output at the end

--- Chapter06/Excercise_1_Code.js
+++ Chapter06/Excercise_1_Code.js
@@ -17,43 +17,6 @@
 // Run
 use sample_mflix
 db.movies.findOne();
-// Will return
- {
-    "_id" : ObjectId("573a1390f29313caabcd4135"),
-    "plot" : "Three men hammer on an anvil and pass a bottle of beer around.",
-    "genres" : ["Short"],
-    "runtime" : 1,
-    "cast" : ["Charles Kayser","John Ott"],
-    "num_mflix_comments" : 1,
-    "title" : "Blacksmith Scene",
-    "fullplot" : "A stationary camera looks at a large anvil with a blacksmith behind it and one on either side. The smith in the middle draws a heated metal rod from the fire, places it on the anvil, and all three begin a rhythmic hammering. After several blows, the metal goes back in the fire. One smith pulls out a bottle of beer, and they each take a swig. Then, out comes the glowing metal and the hammering resumes.",       
-    "countries" : ["USA"],
-    "released" : ISODate("1893-05-09T00:00:00Z"),
-    "directors" : ["William K.L. Dickson"],
-    "rated" : "UNRATED",
-    "awards" : {
-            "wins" : 1,
-            "nominations" : 0,
-            "text" : "1 win."
-    },
-    "lastupdated" : "2015-08-26 00:03:50.133000000",
-    "year" : 1893,
-    "imdb" : {
-            "rating" : 6.2,
-            "votes" : 1189,
-            "id" : 5
-    },
-    "type" : "movie",
-    "tomatoes" : {
-            "viewer" : {
-                    "rating" : 3,
-                    "numReviews" : 184,
-                    "meter" : 32
-            },
-            "lastUpdated" : ISODate("2015-06-28T18:34:09Z")
-    }
-}
-
 // Create Initial Query
 var findTopRomanceMovies = function() {
     print("Finding top Classic Romance Movies...");
@@ -187,5 +150,40 @@ mongo "mongodb+srv://myAtlasCluster-fawxo.gcp.mongodb.net/sample_mflix"
 // Three more documents under this.
 // … 
 
-
+// Output
+ {
+    "_id" : ObjectId("573a1390f29313caabcd4135"),
+    "plot" : "Three men hammer on an anvil and pass a bottle of beer around.",
+    "genres" : ["Short"],
+    "runtime" : 1,
+    "cast" : ["Charles Kayser","John Ott"],
+    "num_mflix_comments" : 1,
+    "title" : "Blacksmith Scene",
+    "fullplot" : "A stationary camera looks at a large anvil with a blacksmith behind it and one on either side. The smith in the middle draws a heated metal rod from the fire, places it on the anvil, and all three begin a rhythmic hammering. After several blows, the metal goes back in the fire. One smith pulls out a bottle of beer, and they each take a swig. Then, out comes the glowing metal and the hammering resumes.",       
+    "countries" : ["USA"],
+    "released" : ISODate("1893-05-09T00:00:00Z"),
+    "directors" : ["William K.L. Dickson"],
+    "rated" : "UNRATED",
+    "awards" : {
+            "wins" : 1,
+            "nominations" : 0,
+            "text" : "1 win."
+    },
+    "lastupdated" : "2015-08-26 00:03:50.133000000",
+    "year" : 1893,
+    "imdb" : {
+            "rating" : 6.2,
+            "votes" : 1189,
+            "id" : 5
+    },
+    "type" : "movie",
+    "tomatoes" : {
+            "viewer" : {
+                    "rating" : 3,
+                    "numReviews" : 184,
+                    "meter" : 32
+            },
+            "lastUpdated" : ISODate("2015-06-28T18:34:09Z")
+    }
+}
 
