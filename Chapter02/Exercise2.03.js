4ae5613afe2f96000f1629e345671c9169f2ce1d
Amit Phaltankar <amitrp@users.noreply.github.com>
2020-08-16 06:40:15 +1000

    Update Exercise2.03.js

--- Chapter02/Exercise2.03.js
+++ Chapter02/Exercise2.03.js
@@ -20,38 +20,38 @@
  *
  */
 {
-	"id": 14253,
-	"Title": "Beauty and the Beast",
-	"year": 2016,
-	"language": "English",
-	"genre": "Romance",
-	"director": "Christophe Gans",
-	"runtime": 112,
-	"imdb": {
-		"rating": 6.4,
-		"votes": "17762"
+    "id": 14253,
+    "Title": "Beauty and the Beast",
+    "year": 2016,
+    "language": "English",
+    "genre": "Romance",
+    "director": "Christophe Gans",
+    "runtime": 112,
+    "imdb": {
+        "rating": 6.4,
+	"votes": "17762"
+    },
+    "tomatoes": {
+	"viewer": {
+	    "rating": 3.9,
+	    "votes": 238
 	},
-	"tomatoes": {
-		"viewer": {
-			"rating": 3.9,
-			"votes": 238
-		},
-		"critic": {
-			"rating": 4.2,
-			"votes": 8
-		},
-		"fresh": 96,
-		"rotten": 7
+	"critic": {
+	    "rating": 4.2,
+	    "votes": 8
 	},
-	"comments": [{
-		"name": "Talisa Maegyr",
-		"email": "oona_chaplin@gameofthron.es",
-		"text": "Rem itaque ad sit rem voluptatibus. Ad fugiat...",
-		"date": "1998-08-22T11:45:03.000+00:00"
-	}, {
-		"name": "Melisandre",
-		"email": "carice_van_houten@gameofthron.es",
-		"text": "Perspiciatis non debitis magnam. Voluptate...",
-		"date": "1974-06-22T07:31:47.000+00:00"
-	}]
+	"fresh": 96,
+	"rotten": 7
+    },
+    "comments": [{
+	"name": "Talisa Maegyr",
+	"email": "oona_chaplin@gameofthron.es",
+	"text": "Rem itaque ad sit rem voluptatibus. Ad fugiat...",
+	"date": "1998-08-22T11:45:03.000+00:00"
+    }, {
+	"name": "Melisandre",
+	"email": "carice_van_houten@gameofthron.es",
+	"text": "Perspiciatis non debitis magnam. Voluptate...",
+	"date": "1974-06-22T07:31:47.000+00:00"
+    }]
 }
