49603e57a528cb055a384b3aa1429544020adfff
Amit Phaltankar <amitrp@users.noreply.github.com>
2020-08-16 06:00:45 +1000

    Update Exercise2.01.js

--- Chapter02/Exercise2.01.js
+++ Chapter02/Exercise2.01.js
@@ -16,7 +16,7 @@
  * Let's assign it to a variable called as `movie`
  *
  */
-var movie = {
+{
   "id" : 14253,
   "title" : "Beauty and the Beast",
   "year" : 2016,
@@ -26,27 +26,3 @@ var movie = {
   "director" : "Christophe Gans",
   "runtime" : 112
 }
-;
-
-/*
- * We will iterate through all the fields in the movie and print the key and value pairs
- */
-
-print("Let's print all the key and value pairs in \"key -> value\" format");
-for (var key in movie) {
-    print(key + " -> " + movie[key]);
-}
-
-/* 
-(Sample output:)
-
-Let's print all the key and value pairs in "key -> value" format
-id -> 14253
-title -> Beauty and the Beast
-year -> 2016
-language -> English
-imdb_rating -> 6.4
-genre -> Romance
-director -> Christophe Gans
-runtime -> 112
-*/
