57224755a7d4ffd8092ff5e6173d554f884eb928
Amit Phaltankar <amitrp@users.noreply.github.com>
2020-08-22 07:45:39 +1000

    Update Exercise_5.01.js

--- Chapter05/Exercise_5.01.js
+++ Chapter05/Exercise_5.01.js
@@ -1,27 +1,25 @@
 /*
  * Amit Phaltankar
- * 29/01/2020
+ * 22/08/2020
  *
  * Packt MongoDB For Begginers.
  * Chapter 5
  */
 
 // Exercise code for Packt MongoDB For Begginers.
-// Exercise 5.01 Delete a low-quality movie
+// Exercise 5.01 Deleting one of many Matched Documents
 
 /**
+  *
   * Your task for this exercise 
-  * 1. Prepare and execute a delete command
-  * 2. The query condition should find movies by IMDb rating of less than 2
-  * 3. The IMDb vote count should be more than 50000
-  * 4. Find all such movies and delete the one with least number of awards
-  * 5. Return the deleted document in respose and include title field. 
+  * 1. Prepare a query with regular expression that matches four documents
+  * 2. Use the query expression in a delete operation and confirm only one document is removed
   * 
   */
-db.movies.findOneAndDelete(
-    {"imdb.rating" : {$lt : 2},"imdb.votes" : {$gt : 50000}},
-    {
-        "sort" : {"awards.won":1},
-        "projection" : {"title" : 1}
-    }
-)
+
+db.new_movies.deleteOne({"title" : {"$regex": "^movie"}})
+
+// Sample Output denotes one document is deleted
+// { "acknowledged" : true, "deletedCount" : 1 }
+
+
